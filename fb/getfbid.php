eca4d07402a54f6b539479e6638d304742bddafd
ttimt <geroginalam@hotmail.com>
2017-05-14 16:28:42 +0800

    fbid

--- fb/getfbid.php
+++ fb/getfbid.php
@@ -1,3 +1,29 @@
 <?php
-    phpinfo();
+    if (isset($_POST['fbusername']))
+    {
+        $username = $_POST['fbusername'];
+        $link = "https://www.facebook.com/";
+        $url = $link . $username;
+
+        $source = file_get_contents($url);
+
+        $pos_entity = strpos($source, "\"entity_id\"");
+        $source_cut = substr($source, $pos_entity);
+
+        $pos_curly = strpos($source_cut, "}");
+        $source_cut = substr($source_cut, $pos_curly);
+
+        echo $source_cut;
+    }
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <h2>Get Facebook ID from username</h2>
+    </head>
+    <body>
+        <form action="" method="post">
+            <input type="text" name="fbusername" placeholder="Enter FB username here:"/>
+        </form>
+    </body>
+</html>
