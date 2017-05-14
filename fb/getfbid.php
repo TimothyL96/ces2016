81e34bee250987347ddd3a934e8cae4a2f740fa5
ttimt <geroginalam@hotmail.com>
2017-05-14 16:39:53 +0800

    add header

--- fb/getfbid.php
+++ fb/getfbid.php
@@ -5,7 +5,13 @@
         $link = "https://www.facebook.com/";
         $url = $link . $username;
 
-        $source = file_get_contents($url);
+        $headers = array(
+            'https' => array(
+                'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36"
+            )
+        );
+        $context = stream_context_create($headers);
+        $source = file_get_contents($url, false, $context);
 
         $pos_entity = strpos($source, "\"entity_id\"");
         $source_cut = substr($source, $pos_entity);
