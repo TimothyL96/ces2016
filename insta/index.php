a57266e0ae4539b0eae5533d965a1ca25937ed4b
ttimt <geroginalam@hotmail.com>
2017-06-04 21:32:30 +0800

    insta

--- insta/index.php
+++ insta/index.php
@@ -14,24 +14,27 @@
 			'code' => $code
 		];
 
-		$accesstoken = curl_init();
+		$caccesstoken = curl_init();
 		$options = array(
 			CURLOPT_URL => $urlaccesstoken,
 			CURLOPT_HEADER => false,
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_POSTFIELDS => $data
 		);
-		curl_setopt_array($accesstoken, $options);
+		curl_setopt_array($caccesstoken, $options);
 
-		$result = curl_exec($accesstoken);
-		curl_close($accesstoken);
+		$result = curl_exec($caccesstoken);
+		curl_close($caccesstoken);
 
-		if (curl_errno($accesstoken))
+		if (curl_errno($caccesstoken))
 			die("Error: 0x0000TK. Contact administrator.");
 
 		$jsonresult = json_decode($result, TRUE);
-		print_r($jsonresult);
+
+		$accesstoken = $jsonresult['access_token'];
 	}
 	if (!isset($code))
 		include_once 'login.php';
+	if (isset($accesstoken))
+		echo 'set';
 ?>
