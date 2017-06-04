f88b6edc2ec428c2aee146272c2462898b622b6f
ttimt <geroginalam@hotmail.com>
2017-06-04 18:11:32 +0800

    insta

--- insta/index.php
+++ insta/index.php
@@ -12,7 +12,7 @@
 		<title>Instagram API TEST</title>
 	</head>
 	<body>
-		<form method="GET" action="api.php">
+		<form method="POST" action="api.php">
 			<input type="submit" name="login" value="LOGIN Instagram"">
 		</form>
 	</body>
