9b87baa1fdb2c5c76b8a78d9767f5da5e8c5622e
ttimt <geroginalam@hotmail.com>
2017-06-04 18:20:12 +0800

    insta

--- insta/index.php
+++ insta/index.php
@@ -1,19 +1,7 @@
 <?php
 	if (isset($_GET['code']))
-	{
 		$code = $_GET['code'];
-		echo $code;
-	}
-?>
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Instagram API TEST</title>
-	</head>
-	<body>
-		<form method="POST" action="api.php">
-			<input type="submit" name="login" value="LOGIN Instagram"">
-		</form>
-	</body>
-</html>
+	if (!isset($code))
+		include_once 'login.php';
+?>
---
+++ insta/login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Instagram API TEST</title>
+	</head>
+	<body>
+		<form method="POST" action="api.php">
+			<input type="submit" name="login" value="LOGIN Instagram"">
+		</form>
+	</body>
+</html>
