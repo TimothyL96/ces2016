4aee598623cc182793ccc303cec86348301b4f00
ttimt <geroginalam@hotmail.com>
2017-06-04 19:14:25 +0800

    insta

--- insta/index.php
+++ insta/index.php
@@ -6,15 +6,13 @@
 		$code = $_GET['code'];
 		$urlaccesstoken = "https://api.instagram.com/oauth/access_token";
 
-		$data = "client_id={$clientID}&client_secret={$clientSecret}&grant_type=authorization_code&redirect_uri={$clientRedirect}&code={$code}";
-/*
 		$data = [
 			'client_id' => $clientID,
 			'client_secret' => $clientSecret,
 			'grant_type' => 'authorization_code',
 			'redirect_uri' => $clientRedirect,
 			'code' => $code
-		];*/
+		];
 
 		$accesstoken = curl_init();
 		$options = array(
@@ -25,17 +23,13 @@
 		);
 		curl_setopt_array($accesstoken, $options);
 
-		echo 'passed   2 ';
 		$result = curl_exec($accesstoken);
 		curl_close($accesstoken);
 
-		echo 'passed3';
 		if (curl_errno($accesstoken))
 			die("Error: 0x0000TK. Contact administrator.");
 
-		echo 'passed4';
-		echo 'error : ' . curl_error($accesstoken);
-		print_r($result);
+		echo $result;
 	}
 	if (!isset($code))
 		include_once 'login.php';
